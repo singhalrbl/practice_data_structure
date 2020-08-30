13df523680a9ed107234ff5f509321ac4d68e1e8
root <root@singhalrbl.localdomain>
2020-08-31 00:50:00 +0530

    replacing package

--- src/geeks/sorting/InsertionSort.java
+++ src/geeks/sorting/InsertionSort.java
@@ -1,13 +1,32 @@
 package geeks.sorting;
 
+/**
+ * InsertionSort
+ */
 public class InsertionSort {
 
-    public static void main(String[] args) {
-        
+    public static void main(final String args[]) {
+        int temp=0;
+        final int arr[] = { 12, 11, 13, 5, 6 };
 
-        
+        for (int i = 1; i < arr.length; i++) {
 
+            for( int j = i ; j > 0  ; j --){
 
+                if(arr[j] < arr[j-1]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }else{
+                    break;
+                }
+
+            }
+
+        }
+        for(int i=0 ; i < arr.length ; i++){
+            System.out.println(arr[i]);
+        }
     }
-    
+
 }
